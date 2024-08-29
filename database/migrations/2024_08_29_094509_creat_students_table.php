5799b58b481750a3301db573e7727191d3b952ed
firstname lastname <jelou8412@gmail.comgit add .git commit -m Activity 1git branch -M maingit remote add origin https://github.com/Rana/ads_Rana_3B_Activitiies.gitgit pish -u origin main>
2024-08-29 18:30:17 +0800

    Activity-2

--- database/migrations/2024_08_29_094509_creat_students_table.php
+++ database/migrations/2024_08_29_094509_creat_students_table.php
@@ -11,13 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('students', function (Blueprint $table) {
-            $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -25,6 +19,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
--- database/migrations/2024_08_29_101937_add_age_to_students_table.php
+++ database/migrations/2024_08_29_101937_add_age_to_students_table.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('students', function (Blueprint $table) {
-            $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamps();
+        Schema::table('students', function (Blueprint $table) {
+            $table->integer('age')->after('email');
+         
         });
     }
 
@@ -25,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropColumn('age')
+        });
     }
 };
